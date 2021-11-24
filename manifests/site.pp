3627db0bca41b4d229801118ba9dc72dbb44d895
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-11-24 10:31:37 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,17 @@
 node default {
-  node 'pmast01.localdomain' {
-    include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => 'This Server/Service is under the control of PUPPET',
+    owner   => 'root',
+  }
+}
+
+node 'pmast01.localdomain' {
+  include role::master_server
+}
+node /^pcli01/ { 
+  include role::app_server
+}
+node /^pcli02/ {
+  include role::db_server
 }
