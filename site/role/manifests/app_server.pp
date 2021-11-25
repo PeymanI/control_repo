aefa2747698048d008c717e09b8d0fae832eccfe
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-11-25 14:42:42 +0000

    Update app_server.pp

--- site/role/manifests/app_server.pp
+++ site/role/manifests/app_server.pp
@@ -2,4 +2,8 @@ class role::app_server {
   include profile::web
   include profile::base
   include profile::app
+  host {'pcli01.localdomain':
+    ensure => present,
+    ip => 192.168.57.102',
+    }
 }
