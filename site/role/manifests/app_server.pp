7b3f1df8a982ea94fb82d10a2d6a9c81a9c27836
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-11-26 10:37:05 +0000

    Update app_server.pp

--- site/role/manifests/app_server.pp
+++ site/role/manifests/app_server.pp
@@ -2,6 +2,13 @@ class role::app_server {
   include profile::web
   include profile::base
   include profile::app
+  
+  file { '/root/README':
+    ensure => file,
+    content => "This Server/Service ${fqdn} is under the control of PUPPET\n",
+    owner   => 'root',
+  }
+  
   host {'pcli01.localdomain':
     ensure => present,
     ip => '192.168.57.102',
