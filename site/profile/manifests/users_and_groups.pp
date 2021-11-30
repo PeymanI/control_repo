850f74e5e0de82d527e75f8d531c325b97aea7e9
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-11-30 11:57:37 +0000

    Update users_and_groups.pp

--- site/profile/manifests/users_and_groups.pp
+++ site/profile/manifests/users_and_groups.pp
@@ -5,14 +5,41 @@ class profile::users_and_groups {
     gid    => '2001',
   }
  
+ group {'project01':
+    ensure => present,
+    gid    => '2002',
+  }
+ 
+ 
   user {'aa_ian':
     ensure     => present,
     uid        => 1001,
-    gid        => 'staff',
+    gid        => 'staff'
+    groups     => 'wheel'
     comment    => 'ian p',
     home       => '/home/aa_ian',
     managehome => true,
   }  
+  
+  user {'aa_ian1':
+    ensure     => present,
+    uid        => 1002,
+    gid        => 'staff',
+    groups     => [ 'wheel','project01' ],
+    comment    => 'ian p - emergency account',
+    home       => '/home/aa_ian1',
+    managehome => true,
+  }  
+  
+  user {'ex103435':
+    ensure     => present,
+    uid        => 2001,
+    gid        => 'project01'
+    comment    => 'Project account 01',
+    home       => '/home/project/ex103435',
+    managehome => true,
+  }  
+  
   
 user {'admin':
     ensure => absent,
