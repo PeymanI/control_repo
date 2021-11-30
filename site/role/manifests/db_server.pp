16fa1ac04f52b6c42e880c526329b4597cae32d9
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-11-30 08:37:45 +0000

    Update db_server.pp

--- site/role/manifests/db_server.pp
+++ site/role/manifests/db_server.pp
@@ -2,12 +2,6 @@ class role::db_server {
   include profile::base
   include profile::db
   
-  file { '/root/README':
-    ensure  => file,
-    content => "This Server/Service ${fqdn} is under the control of PUPPET\n",
-    owner   => 'root',
-  }
-  
   host {'pcli02.localdomain':
     ensure => present,
     ip     => '192.168.57.103',
