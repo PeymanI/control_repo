88a47c732180fb57dd709013803795e987931f51
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-11-30 08:46:32 +0000

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,7 +1,7 @@
 class profile::base {
  file { '/README.PUPPET':
     ensure => file,
-    content => "This Server/Service ${fqdn} is under the control of PUPPET\nUsing PUPPET MASTER pmast01.localdomain",
+    content => "This Server/Service ${fqdn} is under the control of PUPPET\nUsing PUPPET MASTER pmast01.localdomain\n",
     owner   => 'root',
   }
  
