58731aa99d0a0b565ba1081b9ebe86678486f959
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-11-30 11:43:16 +0000

    Update users_and_groups.pp

--- site/profile/manifests/users_and_groups.pp
+++ site/profile/manifests/users_and_groups.pp
@@ -1,6 +1,21 @@
 class profile::users_and_groups {
-  
- user {'admin':
-    ensure => present
+ 
+  group {'staff':
+    ensure => present,
+    gid    => '2001',
   }
+ 
+  user {'admin':
+    ensure => absent,
+  }
+  
+  user {'aa_ian':
+    ensure     => present,
+    uid        => 1001,
+    comment    => 'ian p',
+    home       => '/home/aa_ian',
+    managehome => '/home/aa_ian'
+  }  
+  
+    
 }
