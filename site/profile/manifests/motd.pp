7d704af16110d114eaf09ffe39ea25eb5676907c
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-11-30 10:00:04 +0000

    Update motd.pp

--- site/profile/manifests/motd.pp
+++ site/profile/manifests/motd.pp
@@ -1,7 +1,7 @@
 class profile::motd {
   file { '/etc/motd':
     ensure => file,
-    content => "Property of XXXXX\n\nUnauthorised Access Perhibited\n",
+    content => "Property of XXXXX\n\nUnauthorised Access Prohibited\n",
     owner   => 'root',
     group   => 'root',
     mode    => '644',
