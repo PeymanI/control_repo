c5d03434a01c2c1118ed19c58d87fab366a690aa
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-12-20 14:27:16 +0000

    Update ntp.pp

--- site/profile/manifests/ntp.pp
+++ site/profile/manifests/ntp.pp
@@ -1,3 +1,7 @@
 class profile::ntp {
   include ntp
+  
+  class { 'ntp':
+    [ 'server 0.uk.pool.ntp.org', 'server 1.uk.pool.ntp.org', 'server 2.uk.pool.ntp.org', 'server 3.uk.pool.ntp.org' ],
+  }
  }
