a85ea91d4389366433dc74fd371f90e0ec46a107
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-12-20 14:42:50 +0000

    Update ntp.pp

--- site/profile/manifests/ntp.pp
+++ site/profile/manifests/ntp.pp
@@ -1,10 +1,3 @@
 class profile::ntp {
   include ntp
-    file { '/etc/ntp.conf':
-    ensure => file,
-    content => 'server 0.uk.pool.ntp.org',
-    content => 'server 1.uk.pool.ntp.org',
-    content => 'server 2.uk.pool.ntp.org',
-    content => 'server 3.uk.pool.ntp.org',
-    
-  }
+ }
