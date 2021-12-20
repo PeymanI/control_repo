502e3e6f7c66a2df9573a7adb2481fe160420584
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-12-20 14:36:04 +0000

    Update ntp.pp

--- site/profile/manifests/ntp.pp
+++ site/profile/manifests/ntp.pp
@@ -1,4 +1,10 @@
 class profile::ntp {
   include ntp
-    servers => [ 'server 0.uk.pool.ntp.org', 'server 1.uk.pool.ntp.org', 'server 2.uk.pool.ntp.org', 'server 3.uk.pool.ntp.org' ],
+    file { '/etc/ntp.conf':
+    ensure => file,
+    content => 'server 0.uk.pool.ntp.org',
+    content => 'server 1.uk.pool.ntp.org',
+    content => 'server 2.uk.pool.ntp.org',
+    content => 'server 3.uk.pool.ntp.org',
+    
   }
