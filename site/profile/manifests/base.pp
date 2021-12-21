9c01db4840e0baf64dd6e1d95c7017d42391ad75
PeymanI <94835204+PeymanI@users.noreply.github.com>
2021-12-21 14:48:16 +0000

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -6,6 +6,9 @@ class profile::base {
   }
  
   include profile::ntp
+  class { 'ntp':
+        servers => [ 'server 0.uk.pool.ntp.org', 'server 1.uk.pool.ntp.org', 'server 2.uk.pool.ntp.org', 'server 3.uk.pool.ntp.org' ],
+  }
   include profile::ssh_server
   include profile::motd
   include profile::users_and_groups
